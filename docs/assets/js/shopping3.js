28ba03c8c99322effb007ddc2dad2056e9ad909d
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-06 19:45:52 +0800

    Update shopping3.js
    minor change

--- docs/assets/js/shopping3.js
+++ docs/assets/js/shopping3.js
@@ -247,7 +247,7 @@ console.log(realtimeCart)
 function updateCartItems() {
     cartData = JSON.parse(localStorage.getItem("cartData"))
     localStorage.setItem(realtimeCart, realtimeCart.push(cartData)) //<-- rmb to add back 'window.localstorage...'
-    localStorage.setItem(realtimeCart.flat(1))
+    realtimeCart.flat(1)
     localStorage.setItem("realtimeCart", JSON.stringify(realtimeCart)) //<-- rmb to add back 'window.localstorage...'
     console.log("realtimeCart")
     
