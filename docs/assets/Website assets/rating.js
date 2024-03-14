491a3fe7b543156f9df34f8854dada22948efd9f
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-14 13:23:08 +0800

    Delete docs/assets/Website assets/rating.js

--- docs/assets/Website assets/rating.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-$('.rating_fa-regular fa-heart.r').hover(function() {
-            // get hovered value
-            var rating = $(this).data('rating');
-            var value = $(this).data('value');
-            $(this).parent().attr('class', '').addClass('rating_fa').addClass('rating_'+rating);
-            highlight_star(value);
-        })})
