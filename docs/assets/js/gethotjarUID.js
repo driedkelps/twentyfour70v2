a4f5ff00382b5ce5aafc160e7d758f540f1c1a0f
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 15:44:53 +0800

    Update gethotjarUID.js

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,6 +1,7 @@
+function getHotjarID() {
 var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id
 outputhjUID = hjUID.split("-").shift();
 document.getElementById("hotjarid").textContent = outputhjUID
 
 console.log(document.getElementById("hotjarid").innerText)
-
+}
