0a0ed291b7bf363dcb5b884f3c2eef833c99eed3
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 16:51:28 +0800

    Update gethotjarUID.js
    test function to get hjID at index

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,8 +1,22 @@
-function getHotjarID() {
-if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
+function stringHotjarID() {
+    if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
     outputhjUID = hjUID.split("-").shift();
-    document.getElementById("hotjarid").textContent = outputhjUID;
-  }
-console.log(outputhjUID)
+    localStorage.setItem("sessionID", outputhjUID);
 }
+
+function getHotjarID() {
+    var sessionID = JSON.parse(localStorage.getItem("sessionID");
+    document.getElementById("hotjarid").textContent = sessionID;
+    console.log(sessionID)
+}
+
+
+// function getHotjarID() {
+// if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
+//     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
+//     outputhjUID = hjUID.split("-").shift();
+//     document.getElementById("hotjarid").textContent = outputhjUID;
+//   }
+// console.log(outputhjUID)
+// }
