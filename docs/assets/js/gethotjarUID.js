60a1bbdbeac4522c03f9dc81ed66819dfd0af406
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 17:00:44 +0800

    Update gethotjarUID.js

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,22 +1,23 @@
-function stringHotjarID() {
+var generateIDBtn = document.getElementById('generateHJUID');
+generateIDBtn.addEventListener('click', function(event) {getHotjarID(event)});
+function getHotjarID(event) {
     if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
     outputhjUID = hjUID.split("-").shift();
-    localStorage.setItem("sessionID", outputhjUID);
+    document.getElementById("hotjarid").textContent = outputhjUID;
+     }
+console.log(outputhjUID)
 }
-
-function getHotjarID() {
-    var sessionID = JSON.parse(localStorage.getItem("sessionID");
-    document.getElementById("hotjarid").textContent = sessionID;
-    console.log(sessionID)
-}
-
-
-// function getHotjarID() {
-// if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
-//     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
-//     outputhjUID = hjUID.split("-").shift();
-//     document.getElementById("hotjarid").textContent = outputhjUID;
-//   }
-// console.log(outputhjUID)
-// }
+////////////////////////////
+    // function stringHotjarID() {
+    //     if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
+    //     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
+    //     outputhjUID = hjUID.split("-").shift();
+    //     localStorage.setItem("sessionID", outputhjUID);
+    // }
+    
+    // function getHotjarID() {
+    //     var sessionID = JSON.parse(localStorage.getItem("sessionID");
+    //     document.getElementById("hotjarid").textContent = sessionID;
+    //     console.log(sessionID)
+    // }
