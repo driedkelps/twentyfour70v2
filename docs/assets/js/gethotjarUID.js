dda4c5f96547c84d65ca3e85a87d15717f76a080
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 15:04:29 +0800

    Update gethotjarUID.js
    added window.onload to function

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,5 +1,7 @@
+window.onload = function getHotjarUID() {
 var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds")).id
 outputhjUID = hjUID.split("-").shift();
 document.getElementById("hotjarid").textContent = outputhjUID
 
 console.log(document.getElementById("hotjarid").innerText)
+}
