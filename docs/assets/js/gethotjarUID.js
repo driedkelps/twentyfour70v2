9efd9a66cbf8a94d411d92d622b72229c44893de
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 18:32:37 +0800

    Update gethotjarUID.js
    removed if statement

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,6 +1,5 @@
 var generateIDBtn = document.getElementById('generateHJUID');
 function getHotjarID() {
-    if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
     outputhjUID = hjUID.split("-").shift();
     document.getElementById("hotjarid").textContent = outputhjUID;
