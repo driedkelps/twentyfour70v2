bdf730a708c652e6f0c9e0712439b9d6089078db
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-20 16:17:08 +0800

    Update gethotjarUID.js

--- docs/assets/js/gethotjarUID.js
+++ docs/assets/js/gethotjarUID.js
@@ -1,12 +1,8 @@
-(function getHotjarID() {
-try {
   if (typeof (hj) != "undefined" && hj.hasOwnProperty("globals")) {
     var hjUID = JSON.parse(localStorage.getItem("hjActiveViewportIds"))[0].id;
     outputhjUID = hjUID.split("-").shift();
     document.getElementById("hotjarid").textContent = outputhjUID;
+  }else{
+    continue
   }
-    } catch (err) {
-      /** ignore **/
-    }
-console.log(document.getElementById("hotjarid").innerText)
-})()
+console.log(outputhjUID)
