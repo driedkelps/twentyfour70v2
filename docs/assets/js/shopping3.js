9f854f0ee781d8a28d029fc85da5c11f2e3a8f17
driedkelps <160724620+driedkelps@users.noreply.github.com>
2024-03-31 03:13:51 +0800

    Update shopping3.js

--- docs/assets/js/shopping3.js
+++ docs/assets/js/shopping3.js
@@ -566,7 +566,7 @@ function updateSpendingBar() {
         }
     }else if (leftoverL1 <= 0 && leftoverL1check == -1){
         document.getElementById("spendingBarvalue").textContent = "Not enough currency."
-    } else if{
+    } else if (leftoverL2check != -1 && leftoverL3check != -1 && leftoverL2 >0 && leftoverL3 >0){
         document.getElementById("spendBonusLevel").innerText = "Level 1"
         var newspendPercent = (totalsub / Level1)*100
         document.getElementById("spendingBarvalue").innerText = 'Spend $24.70. | '+'$'+leftoverL1 + ' left!'
